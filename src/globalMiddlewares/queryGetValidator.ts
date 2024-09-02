ad0613c601fccff93035941f28b256f5d2ec2e19
TvSpichkin <ericlen@mail.ru>
2024-09-02 23:10:23 +0300

    Обновление проверки

--- src/globalMiddlewares/queryGetValidator.ts
+++ src/globalMiddlewares/queryGetValidator.ts
@@ -3,7 +3,9 @@ import {SET} from "../settings";
 import {query} from "express-validator";
 
 
-const searchNameTermVal = query("searchNameTerm").optional().isString().withMessage('Имя не является строкой');
+const searchNameTermVal = query("searchNameTerm").optional().isString().withMessage('Поисковый термин для имени не является строкой')
+.trim().isLength({min: 1, max: 15}).withMessage('Поисковый термин для имени содержит больше 15 символов или является пустым'), // Проверка правильности входящего поискового термина для имени
+sortByVal = query("sortBy").optional().isString().withMessage('Поле сортировки не является строкой'); // Проверка правильности входящего поля сортировки
 
 export function queryGetValidator1(req: Request, res: Response, next: NextFunction) {
     const auth = req.headers["authorization"]; // Получение заголовка авторизации из запроса
