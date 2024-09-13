842d7bbf947f3f20eb3bef78fad29a2cd74dc87d
TvSpichkin <ericlen@mail.ru>
2024-09-14 00:40:32 +0300

    Проверка правильности входящего поля сортировки

--- src/IOtypes/queryTypes.ts
+++ src/IOtypes/queryTypes.ts
@@ -1,9 +1,20 @@
 export enum SortDirections {
     asc = "asc", // По возрастанию
     desc = "desc" // По убыванию
-}; // Направление сортировки 
+}; // Направление сортировки
+
+export enum sortByFields {
+    id = "id", // Идентификатор
+    name = "name", // Имя; максимальная длина: 15
+    description = "description", // Описание; максимальная длина: 500
+    websiteUrl = "websiteUrl", // ЕУМР сетевого узла; максимальная длина: 100, шаблон: ^https://([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$
+    createdAt = "createdAt", // Дата создания
+    isMembership = "isMembership", // Подписка на членство в сетевом журнале
+    title = "title", // Название; максимальная длина: 30
+    shortDescription = "shortDescription", // Краткое описание; максимальная длина: 100
+    content = "content", // Содержание; максимальная длина: 1000
+    blogId = "blogId", // Идентификатор существующего сетевого журнала
+}; // Возможные поля сортировки
 
 export type TypeSortDir = keyof typeof SortDirections;
 export type TypeSNT = string | null;
-
-
--- src/globalMiddlewares/queryGetMiddleware.ts
+++ src/globalMiddlewares/queryGetMiddleware.ts
@@ -1,14 +1,23 @@
 import {Request, Response, NextFunction} from "express";
 import {SET} from "../settings";
+import {EntDbType} from "../db/types/typesRepDB";
+import {sortByFields} from "../IOtypes/queryTypes";
 
 
 function checkSNT(snt: string) {
     snt = snt.trim();
-    if(!snt || snt.length > SET.MaxLen.BLOG.NAME) snt = null!;
+    if(!snt || snt.length > SET.MaxLen.BLOG.NAME) snt = undefined!;
 } // Проверка правильности входящего поискового термина для имени
+
+function checkSB(sb: string) {
+    if(!sortByFields.hasOwnProperty(sb)) sb = "createdAt"; // Задание исходного значения для поля сортировки
+} // Проверка правильности входящего поля сортировки
 
 export function queryGetMiddleware(req: Request, res: Response, next: NextFunction) {
     if(typeof req.query.searchNameTerm === "string") checkSNT(req.query.searchNameTerm); // Проверка правильности входящего поискового термина для имени
     
+    if(typeof req.query.sortBy === "string") checkSB(req.query.sortBy); // Проверка правильности входящего поля сортировки
+    else req.query.sortBy = "createdAt"; // Задание исходного значения для поля сортировки
+
     next(); // Передача управления дальше
 }
