36f369748dce0b1ab4302086686dd1c904c74c96
TvSpichkin <ericlen@mail.ru>
2024-09-17 11:33:22 +0300

    Проверка правильности входящего направления сортировки

--- src/globalMiddlewares/queryGetMiddleware.ts
+++ src/globalMiddlewares/queryGetMiddleware.ts
@@ -1,7 +1,7 @@
 import {Request, Response, NextFunction} from "express";
 import {SET} from "../settings";
 import {EntDbType} from "../db/types/typesRepDB";
-import {sortByFields} from "../IOtypes/queryTypes";
+import {sortByFields, SortDirections} from "../IOtypes/queryTypes";
 
 
 function checkSNT(snt: string) {
@@ -10,14 +10,21 @@ function checkSNT(snt: string) {
 } // Проверка правильности входящего поискового термина для имени
 
 function checkSB(sb: string) {
-    if(!sortByFields.hasOwnProperty(sb)) sb = "createdAt"; // Задание исходного значения для поля сортировки
+    if(!sortByFields.hasOwnProperty(sb)) sb = sortByFields.createdAt; // Задание исходного значения для поля сортировки
 } // Проверка правильности входящего поля сортировки
+
+function checkSD(sd: string) {
+    if(!SortDirections.hasOwnProperty(sd)) sd = SortDirections.desc; // Задание исходного значения для поля сортировки
+} // Проверка правильности входящего направления сортировки
 
 export function queryGetMiddleware(req: Request, res: Response, next: NextFunction) {
     if(typeof req.query.searchNameTerm === "string") checkSNT(req.query.searchNameTerm); // Проверка правильности входящего поискового термина для имени
     
     if(typeof req.query.sortBy === "string") checkSB(req.query.sortBy); // Проверка правильности входящего поля сортировки
-    else req.query.sortBy = "createdAt"; // Задание исходного значения для поля сортировки
-
+    else req.query.sortBy = sortByFields.createdAt; // Задание исходного значения для поля сортировки
+    
+    if(typeof req.query.sortDirection === "string") checkSD(req.query.sortDirection); // Проверка правильности входящего направления сортировки
+    else req.query.sortDirection = SortDirections.desc; // Задание исходного значения для направления сортировки
+    
     next(); // Передача управления дальше
 }
