d083541a483a10859b2b631fe67a79a872a65740
TvSpichkin <ericlen@mail.ru>
2024-10-02 12:20:57 +0300

    Рабочая генерация фильтра

--- src/db/repository/createFilter.ts
+++ src/db/repository/createFilter.ts
@@ -2,12 +2,21 @@ import {Filter} from "mongodb";
 import {EntDbType, ProtoFilterType} from "../types/typesRepDB";
 
 
+function valueAssigner(w: ProtoFilterType["way"], v: ProtoFilterType["value"]) {
+    switch(w) {
+        case 1:
+            return {$regex: v};
+        default:
+            return v;
+    }
+} // Присваивание значений
+
 export function createFilter(pf: ProtoFilterType[]): Filter<EntDbType> {
     const f: Filter<EntDbType> = {};
     
     for(let i = 0; i < pf.length; i++) {
-        f[pf[i].key] = pf[i].value;
+        f[pf[i].key] = valueAssigner(pf[i].way, pf[i].value); // Заполнение фильтра
     }
-    
+
     return f;
-}
+} // Генерация фильтра
