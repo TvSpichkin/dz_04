72fb292bb92a25f98cd81feba09a771f3065203d
TvSpichkin <ericlen@mail.ru>
2025-05-06 14:52:40 +0300

    Фикс

--- src/db/repository/createFilter.ts
+++ src/db/repository/createFilter.ts
@@ -23,7 +23,7 @@ export function createFilter(pf: ProtoFilterType[]): Filter<EntDbType> {
     for(let i = 0; i < pf.length; i++) {
         f[pf[i].key] = valueAssigner(pf[i].way, pf[i].value); // Заполнение фильтра
     }
-
+    
     return f;
 } // Генерация фильтра
 
