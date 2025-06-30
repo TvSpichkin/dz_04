9d481a6dc5fcfc51a98c74c91e714acbf317ab3d
TvSpichkin <ericlen@mail.ru>
2025-06-30 22:02:51 +0300

    Фикс

--- src/features/blogs/controllers/getBlogsController.ts
+++ src/features/blogs/controllers/getBlogsController.ts
@@ -10,6 +10,6 @@ import {blogsServ} from "../../../domain/blogsServ";
 export async function getBlogsController(req: ReqQuery<QueryInputModel>, res: Response<Paginator<BlogViewModel>>) {
     const q = req.query,
     sortBy = (blogFields.hasOwnProperty(q.sortBy) ? q.sortBy : blogFields.createdAt) as TypeBlogFields; // Задание исходного значения поля сортировки
-
+    
     res.json(await blogsServ.getAll(q.searchNameTerm, sortBy, q.sortDirection, q.pageNumber, q.pageSize)); // Получение сетевых журналов
 } // Контролёр, отвечающий за выдачу сетевых журналов
